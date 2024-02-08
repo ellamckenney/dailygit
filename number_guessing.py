cff6b7d34abdcf9f8d6aa48ebb06bc45a14dcbb5
Ella McKenney <ellavmckenney@gmail.com>
2024-02-08 11:47:15 -0600

    Add number_guessing.py file

---
+++ number_guessing.py
@@ -0,0 +1,29 @@
+import random
+
+random_number = random.randint(1, 500)
+
+guess_limit = 5
+guesses = 0
+won = False
+
+while guesses < guess_limit and not won:
+  try:
+    guess = int(input("Guess a number between 1 and 500 ({guess_limit} guesses left): "))
+  except ValueError:
+    print("Input is invalid. Please enter a number.")
+    continue
+  
+  guesses += 1
+  if guess == random_number:
+    won = True 
+    print(f"⁺˚⋆｡°✩₊✧˖°˖⁺‧₊˚✦⋆ ˚｡CONGRATS⁺˚⋆｡°✩₊✧˖°˖⁺‧₊˚✦⋆ ˚｡\nYou guessed the number in {guesses} attempts!")
+  elif guess < random_number:
+    print("Your guess is too low, try again!")
+  elif guess > random_number:
+    print("Your guess is too high, try again!")
+    
+  remaining_guesses = guess_limit - guesses
+  print(f"You have {remaining_guesses} guesses left.")
+
+if not won:
+  print(f"You ran out of guesses! The number was {random_number}.")
